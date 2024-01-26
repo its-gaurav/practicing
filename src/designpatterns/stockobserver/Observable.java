8afe27b37ebc2e7db3f342d7a83c00e2c72b7ed9
NAGARRO\gauravraj01 <gaurav.raj01@nagarro.com>
2024-01-26 15:05:57 +0530

    removed license

--- src/designpatterns/stockobserver/Observable.java
+++ src/designpatterns/stockobserver/Observable.java
@@ -1,11 +1,6 @@
 /**
  * Observable.java
- * <p>
- * Copyright (c) 2023 Mercedes-Benz USA, LLC. All Rights Reserved.
- * <p>
- * This software is the confidential and proprietary information of Mercedes-Benz USA, LLC. ("Confidential Information").
- * You shall not disclose such Confidential Information and shall use it only in accordance with the terms you entered into with
- * Mercedes-Benz USA, LLC.
+
  */
 package stockobserver;
 
